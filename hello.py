b82984d26fe9533849bea009a3dacc14e45e4e54
joshps23 <huixmun@gmail.com>
2024-06-30 22:18:52 +0800

    added video analysis

--- hello.py
+++ hello.py
@@ -1,7 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response,jsonify,request,session
 import random
 import datetime
+from flask_wtf import FlaskForm
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired,NumberRange
+import os
+
+import cv2
+
+# YOLO_Video is the python file which contains the code for our object detection model
+#Video Detection is the Function which performs Object Detection on Input Video
+from YOLO_Video import video_detection
+
+from wtforms import FileField, SubmitField
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'anewflaskapp'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+  file = FileField("File",validators=[InputRequired()])
+  submit = SubmitField("Run")
+
+def generate_frames(path_x = ''):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+def generate_frames_web(path_x):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
 
 @app.route('/')
 def hello_world():
@@ -12,6 +48,25 @@ def hello_world():
 @app.route('/anotherpage')
 def get_another_page():
   return render_template("anotherpage.html")
+
+@app.route('/videoanalysis', methods=['GET','POST'])
+def get_video_analysis():
+  form = UploadFileForm()
+  if form.validate_on_submit():
+      # Our uploaded video file path is saved here
+      file = form.file.data
+      file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                              secure_filename(file.filename)))  # Then save the file
+      # Use session storage to save video file path
+      session['video_path'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                            secure_filename(file.filename))
+  return render_template("videoanalysis.html", form=form)
+
+@app.route('/video')
+def video():
+    #return Response(generate_frames(path_x='static/files/bikes.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(path_x = session.get('video_path', None)),mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
   app.run(debug=True)
