b7ed6e1da156105e6712dc0f62d60c7374199280
joshps23 <huixmun@gmail.com>
2024-06-30 21:18:18 +0800

    added bootstrap

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import random
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return render_template("index.html")
+  random_number = random.randint(1, 100)
+  current_year = datetime.datetime.now().year
+  return render_template("index.html", num=random_number, yr=current_year)
+
+@app.route('/anotherpage')
+def get_another_page():
+  return render_template("anotherpage.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
